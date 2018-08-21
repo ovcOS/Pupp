4747a569ab6ae8d7bfa02ed466747ca1a94b3d82
Tom Patton <tom.patton22@gmail.com>
2018-08-21 11:25:44 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'profile/:id', to: 'pages#profile', as: 'profile'
+  resources :puppies do
+    resources :bookings, except: [:edit, :update]
+  end
 end
