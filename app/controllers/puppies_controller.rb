d56aac83117b880c96434ec360cf3b03ba4fe6ab
Oriol VidalCortes <ovidalcortes@gmail.com>
2018-08-21 12:10:38 +0200

    add PuppiesController and BookingsController

---
+++ app/controllers/puppies_controller.rb
@@ -0,0 +1,31 @@
+class PuppiesController < ApplicationController
+  before_action :find_puppy, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def puppy_params
+    params.require(:puppy).permit(:name, :age, :breed_id, :gender, :photo)
+  end
+
+  def find_puppy
+    @puppy = Puppy.find(params[:id])
+  end
+end
