57a50a004e161b1d0bbaaa57340d2b6e291736ac
Nils Lundgren <n.lunpat@gmail.com>
2018-08-22 13:10:16 +0200

    added navbar options and links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_user
 
+private
+
+  def set_current_user
+    @current_user = session[:user_id] ? User.find(session[:user_id]) : nil
+  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
-
-  def current_user
-    if (user_id == session[:user_id]) #here
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id == cookies.signed[:user_id]) #and here
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(:remember, cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
   end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,6 @@ class PagesController < ApplicationController
   end
 
   def profile
-    @user = User.find(params[:id])
+    @user = current_user
   end
 end
