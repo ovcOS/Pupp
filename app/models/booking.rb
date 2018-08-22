0d730aa834afe035a30ffe3501e9c2dbed99b22a
cory bonneau <cory.bonneau2424@gmail.com>
2018-08-22 14:00:56 +0200

    finished implementing booking creation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,7 +7,7 @@ class Booking < ApplicationRecord
 
   belongs_to :puppy
   belongs_to :booker, class_name: "User", foreign_key: :user_id
-  validates :price, :starts_at, :ends_at, presence: true
+  validates :starts_at, :ends_at, presence: true
   validates :price, numericality: { only_integer: true }
   validates :status, inclusion: { in: [PENDING, ACCEPTED, COMPLETED, CANCELED, REJECTED] }
 end
