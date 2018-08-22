d0f00efaf7d660e9f56c306ea27a97db5a8b284c
Nils Lundgren <n.lunpat@gmail.com>
2018-08-22 12:11:01 +0200

    in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,17 @@ class ApplicationController < ActionController::Base
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def current_user
+    if (user_id == session[:user_id]) #here
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id == cookies.signed[:user_id]) #and here
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
 end
