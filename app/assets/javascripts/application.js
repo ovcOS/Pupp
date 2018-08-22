c44f151c40a9750d446c81eeb0293c4f9ef44c04
Tom Patton <tom.patton22@gmail.com>
2018-08-22 09:25:17 +0200

    js update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,9 +4,7 @@
 
 // toggle for the tabs.
 puppy_container = document.querySelector(".pup");
-puppy_button = document.getElementById("puppy_button");
 bookings_container = document.querySelector(".but");
-bookings_button = document.getElementById("bookings_button");
 
 function remove_style(ele) {
   ele.classList.remove("active");
@@ -16,7 +14,7 @@ function add_style(ele) {
   ele.classList.add("active");
 }
 
-puppy_button.addEventListener("click", (event) => {
+puppy_container.addEventListener("click", (event) => {
 // puppy_container.style.classList.remove =(".active");
   remove_style(bookings_container)
   add_style(puppy_container)
@@ -28,12 +26,17 @@ bookings_listings = document.querySelector(".bookings_listings")
 // listings = document.querySelector(".listings")
 
 function hide_html(ele) {
-  ele.hide();
+  ele.style.display = "none";
 }
 
-bookings_button.addEventListener("click", (event) => {
+function show_html(ele) {
+  ele.style.display
+}
+
+bookings_container.addEventListener("click", (event) => {
 // puppy_container.style.classList.remove =(".active");
   remove_style(puppy_container)
   add_style(bookings_container)
-  hide_html(bookings_listings)
+  hide_html(puppy_listings)
+  show_html(bookings_listings)
 });
