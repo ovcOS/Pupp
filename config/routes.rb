36a5d4711298a14f7a9eccf12143ab0bb519dbcd
cory bonneau <cory.bonneau2424@gmail.com>
2018-08-23 20:26:52 +0200

    fixed profile issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'profile/:id', to: 'pages#profile', as: 'profile'
+  get 'profile', to: 'pages#profile', as: 'profile'
+  get
   resources :puppies do
     resources :bookings, except: [:index, :edit, :update]
   end
