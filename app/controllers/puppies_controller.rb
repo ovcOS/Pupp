700807a93cc06fffd12580805e3127f225e5201b
cory bonneau <cory.bonneau2424@gmail.com>
2018-08-24 03:06:17 +0200

    done

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -24,6 +24,7 @@ class PuppiesController < ApplicationController
 
   def create
     @puppy = Puppy.create(puppy_params)
+    @puppy.photo = "http://www.placepuppy.net/#{rand(1..20)}p/1300/600"
     @user_id = current_user.id
     @puppy.user_id = @user_id
 
